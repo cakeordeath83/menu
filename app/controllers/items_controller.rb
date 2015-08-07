250f9a63ce2b239a8906b899ff275b891749c58f
Clair Griffiths <clair_griffiths@hotmail.com>
2015-08-07 09:00:26 +0000

    sorted out some routing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,34 +1,30 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :find_retailer
 
-  # GET /items
-  # GET /items.json
+  
   def index
-    @items = Item.all
   end
 
-  # GET /items/1
-  # GET /items/1.json
   def show
+    @item = @retailer.items.find(params[:id])
   end
 
-  # GET /items/new
+  
   def new
-    @item = Item.new
+    @item = @retailer.items.new
   end
 
-  # GET /items/1/edit
   def edit
+    @item = @retailer.items.find(params[:id])
   end
 
-  # POST /items
-  # POST /items.json
   def create
-    @item = Item.new(item_params)
+    @item = @retailer.items.new(item_params)
 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        # May need to sort the @item part to retailer_item_path
+        format.html { redirect_to retailer_items_path, notice: 'Item was successfully created.' }
         format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new }
@@ -37,12 +33,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /items/1
-  # PATCH/PUT /items/1.json
   def update
+    @item = @retailer.items.find(params[:id])
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.html { redirect_to retailer_items_path, notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit }
@@ -51,20 +46,21 @@ class ItemsController < ApplicationController
     end
   end
 
-  # DELETE /items/1
-  # DELETE /items/1.json
   def destroy
+    @item = @retailer.items.find(params[:id])
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.html { redirect_to retailer_items_path, notice: 'Item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = Item.find(params[:id])
+    
+  
+    def find_retailer
+      @retailer = Retailer.find(params[:retailer_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
