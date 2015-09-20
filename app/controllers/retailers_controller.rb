1a479ca1f0952aad9d646a846cead53aec5f4e8c
Clair Griffiths <clair_griffiths@hotmail.com>
2015-09-20 11:55:57 +0000

    Put Category dropdown back

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -70,7 +70,7 @@ class RetailersController < ApplicationController
     end
   end
   
-  # Codebar - doesn't seem to work in view
+  # Codebar - doesn't seem to work when I try and run it in the view
   def postcode_given
     if params[:search] =~ /^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/
       true
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ActiveRecord::Base
   accepts_nested_attributes_for :category 
   validates_presence_of :name, :price, :category
   
-   
+  # codebar! If the validation doesn't pass, it won't display the dropdown with the categories in it
   validates_uniqueness_of :name
   #validates_numericality_of :price, :less_than => 10, :message => "must be less than £10. Did you add a decimal point?"
     
