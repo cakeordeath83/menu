3c64f9550c7e1c727e018864066d5f1b6c152711
Clair Griffiths <clair_griffiths@hotmail.com>
2015-09-24 07:29:45 +0000

    fixed increase number and category dropdown

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,20 +5,19 @@ class Category < ActiveRecord::Base
   validates_uniqueness_of :category
   has_many :items
   
- before_save :increase_number
+  after_save :increase_number
   
   #CODEBAR!
   # Running this seems to increase the numbers in a weird way
   def increase_number 
-    @categories = Category.all 
+    Category.where("order_number >= ? and id != ?", self.order_number, self.id).update_all("order_number = order_number + 1")
     
-    @categories.each{|c| 
-      
-      if c.order_number >= self.order_number
-        c.increment!(:order_number)
-                
-      end}
     
-  end
+    #@categories.each{|c| 
+    #  if c.order_number == self.order_number
+    #    c.increment!(:order_number)
+    #  end}
+    
+ end
   
 end
